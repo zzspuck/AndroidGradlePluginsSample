003b4dc5820189e614e0ac1daa043e0cd67a2152
zzspuck <zzspuck@gmail.com>
2024-01-17 22:17:05 +0800

    修改插件修改

--- CustomGradlePlugin/src/main/java/com/zzs/plugin/CustomGradlePlugin.java
+++ CustomGradlePlugin/src/main/java/com/zzs/plugin/CustomGradlePlugin.java
@@ -1,4 +1,14 @@
 package com.zzs.plugin;
 
-public class CustomGradlePlugin {
+
+import static org.codehaus.groovy.runtime.DefaultGroovyMethods.println;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class CustomGradlePlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project target) {
+        println("CustomGradlePlugin");
+    }
 }
