00cffb6b86f381e3790bd4cd67896fc7bc27d55b
zzs <761120751@qq.com>
2024-01-18 18:44:44 +0800

    buildSrc方式集成Gradle插件

---
+++ StandaloneGradlePlugin/src/main/java/com/zzs/plugins/GreetingStandaloneGradlePlugins.java
@@ -0,0 +1,17 @@
+package com.zzs.plugins;
+
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class GreetingStandaloneGradlePlugins implements Plugin<Project> {
+
+    private static final String TAG = "GreetingStandaloneGradlePlugins";
+
+    @Override
+    public void apply(Project project) {
+        System.out.println("GreetingStandaloneGradlePlugins(standalone) ---> apply");
+        project.task("helloStandalone").doLast(task -> System.out.println("Hello from the com.nxg.plugins.GreetingStandaloneGradlePlugins(standalone)"));
+    }
+}
+
