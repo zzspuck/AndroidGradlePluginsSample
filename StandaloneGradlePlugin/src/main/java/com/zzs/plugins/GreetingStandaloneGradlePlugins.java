cbe7813adef4cbfea51af831fe0303ce714168ca
zzs <761120751@qq.com>
2024-03-11 19:03:11 +0800

    添加页面可以进行测试了

--- StandaloneGradlePlugin/src/main/java/com/zzs/plugins/GreetingStandaloneGradlePlugins.java
+++ StandaloneGradlePlugin/src/main/java/com/zzs/plugins/GreetingStandaloneGradlePlugins.java
@@ -1,14 +1,17 @@
 package com.zzs.plugins;
 
 import com.android.build.api.instrumentation.FramesComputationMode;
+import com.android.build.api.instrumentation.InstrumentationParameters;
 import com.android.build.api.instrumentation.InstrumentationScope;
 import com.android.build.api.variant.AndroidComponentsExtension;
+import com.android.build.api.variant.ApplicationVariant;
 import com.android.build.api.variant.VariantSelector;
 
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
+import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
 
 public class GreetingStandaloneGradlePlugins implements Plugin<Project> {
@@ -18,14 +21,11 @@ public class GreetingStandaloneGradlePlugins implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         AndroidComponentsExtension androidComponents = project.getExtensions().getByType(AndroidComponentsExtension.class);
-       /* androidComponents.onVariants(androidComponents.selector().all(),variant -> {
-            variant.getInstrumentation().transformClassesWith(
-                    FooClassVisitorFactory.class, InstrumentationScope.ALL,
-                    it -> {
-                    }
-            );
+
+        androidComponents.onVariants(androidComponents.selector().all(), (Action<ApplicationVariant>) variant -> {
+            variant.getInstrumentation().transformClassesWith(FooClassVisitorFactory.class, InstrumentationScope.ALL, none -> null);
             variant.getInstrumentation().setAsmFramesComputationMode(FramesComputationMode.COPY_FRAMES);
-        });*/
+        });
     }
 }
 
