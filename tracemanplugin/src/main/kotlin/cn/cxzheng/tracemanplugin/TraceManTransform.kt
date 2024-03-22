d53dd107a074f2807bd6d75fa164ee4abc5729dd
zzs <761120751@qq.com>
2024-03-22 18:25:09 +0800

    提交代码

--- tracemanplugin/src/main/kotlin/cn/cxzheng/tracemanplugin/TraceManTransform.kt
+++ tracemanplugin/src/main/kotlin/cn/cxzheng/tracemanplugin/TraceManTransform.kt
@@ -3,7 +3,9 @@ package cn.cxzheng.tracemanplugin
 import com.android.build.api.instrumentation.AsmClassVisitorFactory
 import com.android.build.api.instrumentation.ClassContext
 import com.android.build.api.instrumentation.ClassData
+import com.android.tools.lint.LintResourceRepository.Companion.EmptyRepository.project
 import org.objectweb.asm.ClassVisitor
+import java.io.File
 
 abstract class TraceManTransform():AsmClassVisitorFactory<TraceManParameter> {
 
@@ -11,6 +13,11 @@ abstract class TraceManTransform():AsmClassVisitorFactory<TraceManParameter> {
         classContext: ClassContext,
         nextClassVisitor: ClassVisitor
     ): ClassVisitor {
+        val traceManConfig = parameters.get()
+        val output = traceManConfig.output.get()
+                if (output == null || output.isEmpty()) {
+                    //traceManConfig.output.set() = project.getBuildDir().getAbsolutePath() + File.separator + "traceman_output"
+                }
       return TraceClassVisitor(nextClassVisitor,parameters.get())
     }
 
